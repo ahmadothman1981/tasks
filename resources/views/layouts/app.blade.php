d347f76fbb31d3cf2c638ff3ab56153dec46b5a0
ahmadothman1981 <ahmadothman@outlook.com>
2024-07-04 21:02:34 +0300

    add flash messages using alpine js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,8 @@
     <head>
         <title>Task List</title>
         <script src="https://cdn.tailwindcss.com"></script>
+        <script src="//unpkg.com/alpinejs" defer></script>
+
 
         {{-- blade-formatter-disable --}}
         <style type="text/tailwindcss">
@@ -29,11 +31,22 @@
 
     <body class="container mx-auto mt-10 mb-10 max-w-lg">
         <h1 class="text-2xl mb-4">@yield('title')</h1>
-        <div>
-            @if(session()->has('success'))
-            <div>{{session('success')}}</div>
-            @endif
+        <div x-data="{flash:true}">
+           @if(session()->has('success'))  
+          
+           <div role="alert" x-show="flash"
+            class=" relative mb-10 rounded border border-green-400 bg-green-100 px-4 py-3 text-lg text-green-700"><strong class="font-bold">
+            Success!</strong><div>{{session('success')}}</div>
+
+            <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+            stroke-width="1.5" @click="flash = false"
+            stroke="currentColor" class="h-6 w-6 cursor-pointer">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </span>
         </div>
+        @endif
         <div>@yield('content')</div>
     </body>
 </html>
