5c2c4e9bcc78975ddfb80e71bcbefe21549e742c
ahmadothman1981 <ahmadothman@outlook.com>
2024-07-04 12:44:02 +0300

    styling with tailwind css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,19 +2,20 @@
 
 @section('title','List Of Tasks')
 @section('content')
-<div>
-    <a href="{{route('task.create')}}">Create Task</a>
-</div>
+<nav class="mb-4">
+    <a class="font-medium text-gray-700 underline decoration-pink-500" href="{{route('task.create')}}">Create Task</a>
+</nav>
     @forelse($tasks as $task)
 
     <div>
-        <a href="{{route('task.show',$task->id)}}">{{$task->title}}</a>
+        <a  href="{{route('task.show',$task->id)}}"
+         @class(['line-through'=>$task->completed])>{{$task->title}}</a>
     </div>
     @empty
     <div>There is No Tasks</div>
     @endforelse
 
     @if($tasks->count())
-    <nav>{{$tasks->links()}}</nav>
+    <nav class="mt-4">{{$tasks->links()}}</nav>
     @endif
     @endsection
